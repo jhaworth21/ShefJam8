edb1e38e68b452399962ea683cb1dcf4089d4b2a
jhaworth21 <jhaworth21@gmail.com>
2022-02-11 23:21:29 +0000

    added background and midground

--- src/js/eyes/main.js
+++ src/js/eyes/main.js
@@ -1,24 +1,38 @@
 import * as PIXI from 'pixi.js'
 import { Renderer } from 'pixi.js';
 
-const app = new PIXI.Application();
+const app = new PIXI.Application({width: window.innerWidth,height: window.innerHeight, resizeTo: window});
+document.body.appendChild(app.view);
+// stage = new PIX  .Container();
 
-stage = new PIXI.Container();
+// var width = document.getElementById("game-canvas").width;
+var width = window.innerWidth;
+// var height = document.getElementById("game-canvas").height;
+var height = window.innerHeight;
 
-var width = document.getElementById("game-canvas").width;
-var height = document.getElementById("game-canvas").height;
+renderer = PIXI.autoDetectRenderer(width, height, {view:document.getElementById("game-canvas")});
 
-renderer = PIXI.autoDetectRenderer(width, height, {view:document.getElementById("game-canvas")})
-
-
-var bg_texture = PIXI.Texture.from("src\assets\Background\Free Pixel Art Forest\Free Pixel Art Forest\Preview\Background.png");
+const bg_texture = PIXI.Texture.from(require("../../assets/Background/3layers/Background.png"));
 bg = new PIXI.Sprite(bg_texture);
 
+const mg_texture = PIXI.Texture.from(require("../../assets/Background/3layers/Midground.png"));
+mg = new PIXI.Sprite(mg_texture);
+
 bg.position.x = 0;
-bg.position.y = 0;
+bg.position.y = height-750;
 
-stage.addChild(bg);
+bg.width = window.innerWidth;
 
-renderer.render(stage);
+mg.position.x = 0;
+mg.position.y = height-750;
+mg.width = window.innerWidth;
+
+app.stage.addChild(bg);
+app.stage.addChild(mg);
+
+// stage.addChild(bg);
+// stage.addChild(mg);
+
+renderer(stage);
 
 console.log(app);
