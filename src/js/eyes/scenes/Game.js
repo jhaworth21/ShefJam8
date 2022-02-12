0a28335db2188ebad3273a058103cca8bfdfa4d5
Thomas Milburn <milburn.j.thomas@gmail.com>
2022-02-12 06:03:59 +0000

    Got scrolling working!!

--- src/js/eyes/scenes/Game.js
+++ src/js/eyes/scenes/Game.js
@@ -2,6 +2,11 @@ import Phaser, {Textures} from 'phaser';
 
 export default class Game extends Phaser.Scene {
 
+   constructor() {
+      super({});
+      this.largest_background_offset = 1;
+   }
+
    preload() {
       this.load.image("bg", require("../../../assets/Background/3layers/Background.png"));
       // this.load.image("mg", "../../assets/Background/3layers/Midground.png");
@@ -9,13 +14,25 @@ export default class Game extends Phaser.Scene {
    }
 
    create() {
-      const width = this.scale.width;
       const height = this.scale.height;
 
-      this.add.image(0, height, "bg").setOrigin(0, 1).setScale();
+      this.bg = this.add.tileSprite(0, height, 1080, 1080, "bg").setOrigin(0, 1);
+      let scaleX = this.cameras.main.width / this.bg.width;
+      this.bg.setScale(scaleX);
+
       // const mg = this.add.image(200, 200, "mg")
       // mg.setScale(1.0);
       // const fg = this.add.image(200, 200, "fg")
       // fg.setScale(1.0);
+   }
+
+   update(time, delta) {
+      const cursors = this.input.keyboard.createCursorKeys();
+
+      if (cursors.left.isDown) {
+         this.bg.tilePositionX -= 1;
+      } else if (cursors.right.isDown) {
+         this.bg.tilePositionX += 1;
+      }
    }
 }
